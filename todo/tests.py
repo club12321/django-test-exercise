7bb2f6fe7d0f72e70d86e40b685809aeddcc7933
1f10230303 <s1f102303035@iniad.org>
2024-06-27 11:59:32 +0900

    テスト追加

--- todo/tests.py
+++ todo/tests.py
@@ -35,3 +35,12 @@ class TaskMOdelTestCase(TestCase):
         task.save()
 
         self.assertFalse(task.is_overdue(current))
+
+class TodoViewTestCase(TestCase):
+    def test_index_get(self):
+        client = Client()
+        response = client.get("/")
+
+        self.assertEqual(reaponse.status_code, 200)
+        self.assertEqual(response.templates[0].name, "todo/index.html")
+        self.assertEqual(len(response.context["Tasks"], 0))
